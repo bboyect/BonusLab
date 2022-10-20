c60a6b0d22f1ecd66bd3f3666a40b343c35a5e9b
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-10-20 13:22:56 +0300

    a note should be added for the test

--- tests/testthat/test-BonusLab.R
+++ tests/testthat/test-BonusLab.R
@@ -11,5 +11,8 @@ test_that("lenreg rejects errounous input", {
 test_that("The coefficients are right", {
   
   mod_object <- ridgereg(Petal.Length~Species, data = iris, lambda=0)
-  expect_equal(mod_object$regressions_coefficients, MASS::lm.ridge(Petal.Length~Species, data = iris))
+  # From running MASS::lm.ridge(Petal.Length~Species, data = iris, we get 1.462, 2.798 and 4.090
+  expect_equal(all.equal(mod_object$regressions_coefficients[1], 1.462), TRUE)
+  expect_equal(all.equal(mod_object$regressions_coefficients[2], 2.798), TRUE)
+  expect_equal(all.equal(mod_object$regressions_coefficients[3], 4.090), TRUE)
 })
