60352fc55f6205bc7b79f996894469d6813ac94d
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-10-20 13:07:49 +0300

    Created test. We need to add comment for this test

--- tests/testthat/test-BonusLab.R
+++ tests/testthat/test-BonusLab.R
@@ -6,3 +6,10 @@ test_that("lenreg rejects errounous input", {
   expect_error(linreg_mod <- ridgereg$new(formula = Petal.Length~Sepdsal.Width+Sepal.Length, data=iris,1))
   expect_error(linreg_mod <- ridgereg$new(formula = Petal.Length~Sepdsal.Width+Sepal.Length, data=irfsfdis))
 })
+
+
+test_that("The coefficients are right", {
+  
+  mod_object <- ridgereg(Petal.Length~Species, data = iris, lambda=0)
+  expect_equal(mod_object$regressions_coefficients, MASS::lm.ridge(Petal.Length~Species, data = iris))
+})
